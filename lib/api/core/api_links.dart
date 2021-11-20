5c765528647f3b846472c98fe3d4620d90aa5e18
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 09:30:52 +0530

    FEATURE: Added Api Links

--- lib/api/core/api_links.dart
+++ lib/api/core/api_links.dart
@@ -1,1 +1,9 @@
-class ApiLinks {}
+class ApiLinks {
+  static const bool isStaging = false;
+  static const String _devBase = "https://watbackend.herokuapp.com";
+  static const String _prodBase = "";
+
+  final String baseUrl = "${isStaging ? _prodBase : _devBase}/api/v1";
+
+  String get websiteCheckApi => "/pa11y";
+}
