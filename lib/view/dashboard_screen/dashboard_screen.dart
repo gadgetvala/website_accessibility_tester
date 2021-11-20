a89ed75b24edeed23f812cc380b0c399e678c30b
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 10:46:59 +0530

    FEATURE: Added data from api on Dashboard Screen

--- lib/view/dashboard_screen/dashboard_screen.dart
+++ lib/view/dashboard_screen/dashboard_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wat/res/res.dart';
+import 'package:wat/view/dashboard_screen/provider/dashboard_provider.dart';
 import 'package:wat/view/dashboard_screen/widget/listview_issues.dart';
 import 'package:wat/view/dashboard_screen/widget/search_header.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends ConsumerWidget {
   static const String id = '/dashboardScreen';
 
   const DashboardScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final _provider = watch(dashboardProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -29,32 +33,54 @@ class DashboardScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SearchHeader(),
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  RichText(
-                    text: TextSpan(
-                      text: 'Suraj Verma PORTFOLIO',
-                      style: TextStyle(
-                        color: colors.primaryFontColor,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: ' : 20 Issue Found',
-                          style: TextStyle(
-                            color: colors.secondaryColor,
-                          ),
-                        ),
-                      ],
+            if (_provider.isLoading)
+              Expanded(
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: colors.secondaryColor,
+                  ),
+                ),
+              ),
+            if (!_provider.isLoading && _provider.data == null)
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'Enter website name to search',
+                    style: TextStyle(
+                      color: colors.primaryFontColor,
+                      fontSize: 20,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  ListViewIssues(),
-                ],
+                ),
               ),
-            ),
+            if (_provider.data != null)
+              Expanded(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    RichText(
+                      text: TextSpan(
+                        text: _provider.data!.documentTitle,
+                        style: TextStyle(
+                          color: colors.primaryFontColor,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                ' : ${_provider.data!.issues.length} Issues Found',
+                            style: TextStyle(
+                              color: colors.secondaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ListViewIssues(),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
