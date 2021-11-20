d7682b5abcb0ece0ef59e4f803d6fa2978ff612b
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 19:49:39 +0530

    FEATURE: Added Dark Mode and Light Mode Color Scheme

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  final backgroundColor = const Color(0xffffffff);
-  final primaryColor = const Color(0xfff0f5f9);
-  final secondaryColor = const Color(0xff784ffe);
-  final primaryFontColor = const Color(0xff002237);
-  final secondaryfontColor = const Color(0xffffffff);
+  late Color backgroundColor;
+  late Color primaryColor;
+  late Color secondaryColor;
+  late Color primaryFontColor;
+  late Color borderColor;
+
+  void initializeColor({required bool dark}) {
+    backgroundColor = dark ? Color(0xff141a1e) : Color(0xffffffff);
+    primaryColor = dark ? Color(0xff202a30) : Color(0xfff0f5f9);
+    secondaryColor = const Color(0xff784ffe);
+    primaryFontColor = dark ? Color(0xffffffff) : Color(0xff002237);
+    borderColor = dark ? Color(0xff202a30) : Color(0xffffffff);
+  }
 }
