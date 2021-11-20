f8bb9a9c34083ccf49c189394a3b4f602aae539b
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 10:44:57 +0530

    FEATURE: Export Pa11y API

--- lib/api/core/api_exporter.dart
+++ lib/api/core/api_exporter.dart
@@ -1,4 +1,4 @@
 import 'package:wat/api/core/api_core.dart';
-import 'package:wat/api/random_api.dart';
+import 'package:wat/api/pa11y_api.dart';
 
-class ApiExporter with ApiCore, RandomApi {}
+class ApiExporter with ApiCore, Pa11yApi {}
