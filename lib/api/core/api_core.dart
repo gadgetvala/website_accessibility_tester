a9d76879a8f9aace94b8eac1500ad40e2f82f396
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 09:30:29 +0530

    REFACTOR: Remove UNsed Items

--- lib/api/core/api_core.dart
+++ lib/api/core/api_core.dart
@@ -1,17 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:wat/api/core/api_links.dart';
-import 'package:wat/global/shared_preferences.dart';
 import 'package:wat/locator/locator.dart';
 
 class ApiCore {
-  static final Preferences _prefs = locator<Preferences>();
   static final ApiLinks _apiLinks = locator<ApiLinks>();
   final Dio apiClient = Dio(
     BaseOptions(
       baseUrl: _apiLinks.baseUrl,
-      headers: {
-        'Authorization': 'Bearer ${_prefs.token}',
-      },
     ),
   );
 
