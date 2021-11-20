c1c3349fe15d7fa000d035e3c5566197e434b22c
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 19:50:44 +0530

    FEATURE: Added Settings Icon on Dashboard Screen

--- lib/view/dashboard_screen/dashboard_screen.dart
+++ lib/view/dashboard_screen/dashboard_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wat/provider/theme_provider.dart';
 import 'package:wat/res/res.dart';
 import 'package:wat/view/dashboard_screen/provider/dashboard_provider.dart';
 import 'package:wat/view/dashboard_screen/widget/listview_issues.dart';
 import 'package:wat/view/dashboard_screen/widget/search_header.dart';
+import 'package:wat/view/settings_screen/settings_screen.dart';
 
 class DashboardScreen extends ConsumerWidget {
   static const String id = '/dashboardScreen';
@@ -13,6 +15,7 @@ class DashboardScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final _provider = watch(dashboardProvider);
+    final _ = watch(themeProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -26,7 +29,23 @@ class DashboardScreen extends ConsumerWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(SettingsScreen.id);
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(
+                Icons.settings,
+                color: colors.secondaryColor,
+                size: 25,
+              ),
+            ),
+          )
+        ],
       ),
+      backgroundColor: colors.backgroundColor,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
