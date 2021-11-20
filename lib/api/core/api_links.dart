7b715c7f80804665c90d21cee1385cdb9ad2c575
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 10:45:46 +0530

    FEATURE: Added DEV Url For server

--- lib/api/core/api_links.dart
+++ lib/api/core/api_links.dart
@@ -1,6 +1,6 @@
 class ApiLinks {
   static const bool isStaging = false;
-  static const String _devBase = "https://watbackend.herokuapp.com";
+  static const String _devBase = "http://10.0.2.2:3000";
   static const String _prodBase = "";
 
   final String baseUrl = "${isStaging ? _prodBase : _devBase}/api/v1";
