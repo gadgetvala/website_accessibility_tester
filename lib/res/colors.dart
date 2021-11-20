30a53bc8638969c9bd3f9ae9b748b0dad469c0e4
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 09:31:17 +0530

    FEATURE: Added Color Scheme

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  final backgroundColor = const Color(0xffE5E5E9);
+  final backgroundColor = const Color(0xffffffff);
+  final primaryColor = const Color(0xfff0f5f9);
+  final secondaryColor = const Color(0xff784ffe);
+  final primaryFontColor = const Color(0xff002237);
+  final secondaryfontColor = const Color(0xffffffff);
 }
