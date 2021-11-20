8f482773db5c449eb1c69935d9e5f5c536f19f23
gadgetvala <surajj.verma7@gmail.com>
2021-11-20 10:48:18 +0530

    REFACTOR: Remove unsed api

--- lib/api/random_api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-import 'package:wat/api/core/api_core.dart';
-import 'package:wat/api/core/api_links.dart';
-import 'package:wat/api/core/custom_response.dart';
-import 'package:wat/locator/locator.dart';
-
-mixin RandomApi on ApiCore {
-  final ApiLinks endpoint = locator<ApiLinks>();
-}
