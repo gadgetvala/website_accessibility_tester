f50c5565538c20a8494c0414955a4888abf82ec6
gadgetvala <surajj.verma7@gmail.com>
2021-12-05 18:27:50 +0530

    FIX: Remove Unsed Mount

--- 2.flutter/lib/api/core/api_links.dart
+++ 2.flutter/lib/api/core/api_links.dart
@@ -1,7 +1,7 @@
 class ApiLinks {
-  static const bool isStaging = false;
+  static const bool isStaging = true;
   static const String _devBase = "http://10.0.2.2:3000";
-  static const String _prodBase = "";
+  static const String _prodBase = "http://f664-103-228-147-154.ngrok.io/";
 
   final String baseUrl = "${isStaging ? _prodBase : _devBase}/api/v1";
 
