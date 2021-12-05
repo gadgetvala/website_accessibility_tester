036f29f3c48fb0c20556fb00b31036e3c2f5a7aa
gadgetvala <surajj.verma7@gmail.com>
2021-12-05 14:32:33 +0530

    Remove Plain js Server and Added Typescript Server

--- plain_js/routers/pa11yRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const pa11yController = require("./../controllers/pa11yController");
-
-router.route("/").get(pa11yController.checkWebsite);
-
-module.exports = router;
--- plain_js/utils/haltOnTimedout.js
+++
@@ -1,3 +0,0 @@
-module.exports = function haltOnTimedout(req, res, next) {
-  if (!req.timedout) next();
-};
