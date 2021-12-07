6c33258af8dde5ec369e32d8c191aceb9df35bd4
gadgetvala <surajj.verma7@gmail.com>
2021-12-07 13:57:38 +0530

    FEATURE: Added App Theme

---
+++ 3.nextjs/styles/theme.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+interface ThemeProps {
+  backgroundColor: string;
+  primaryColor: string;
+  secondaryColor: string;
+  primaryFontColor: string;
+  borderColor: string;
+}
+
+export const lightTheme: ThemeProps = {
+  backgroundColor: "#ffffff",
+  primaryColor: "#f0f5f9",
+  secondaryColor: "#784ffe",
+  primaryFontColor: "#002237",
+  borderColor: "#ffffff",
+};
+
+export const darkTheme: ThemeProps = {
+  backgroundColor: "#141a1e",
+  primaryColor: "#202a30",
+  secondaryColor: "#784ffe",
+  primaryFontColor: "#ffffff",
+  borderColor: "#202a30",
+};
+
+export const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: "Dosis";
+        src: url("/fonts/Dosis.ttf");
+    }
+
+    *,
+    *::after,
+    *::before{
+        padding: 0;
+        margin: 0;
+    }
+    box-sizing: border-box;
+
+    body {
+      font-family: "Dosis";
+    }
+`;
