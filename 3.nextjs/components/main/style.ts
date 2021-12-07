4bd16a201012adbdf29dbaa381da38784656e725
gadgetvala <surajj.verma7@gmail.com>
2021-12-07 14:02:17 +0530

    FEATURE: Added Styles for Main Component

---
+++ 3.nextjs/components/main/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+export const WidthContainer = styled.div`
+  max-width: 1000px;
+  min-height: 95vh;
+  margin: 0 auto;
+  padding: 2rem 1rem 2rem 1rem;
+`;
+
+export const EmptyDataContainer = styled.div`
+  min-height: 400px;
+  max-height: 600px;
+  display: flex;
+  color: ${(props) => props.theme.primaryFontColor};
+  font-weight: 500;
+  font-size: 1.3rem;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 2rem 1rem 2rem 1rem;
+`;
