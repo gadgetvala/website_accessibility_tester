9fe05d8406946a447818e7eab1f44401db9079f8
gadgetvala <surajj.verma7@gmail.com>
2021-12-08 12:09:41 +0530

    FEATURE: Added Result And Search State

--- 3.nextjs/context/GlobalContext.tsx
+++ 3.nextjs/context/GlobalContext.tsx
@@ -5,14 +5,17 @@ import {
   Dispatch,
   SetStateAction,
 } from "react";
+import { ApiResponse } from "../types/api_res_types";
 import { ThemeTypeEnum } from "../types/themeTypes";
 
 // interface
 interface GlobalContextInterface {
   currentTheme: ThemeTypeEnum;
   setCurrentTheme: Dispatch<SetStateAction<ThemeTypeEnum>>;
-  result: [];
-  setResult: Dispatch<SetStateAction<[]>>;
+  result: ApiResponse | null;
+  setResult: Dispatch<SetStateAction<ApiResponse | null>>;
+  loading: boolean;
+  setLoading: Dispatch<SetStateAction<boolean>>;
 }
 
 // Context
@@ -23,13 +26,16 @@ export const GlobalProvider = ({ children }: { children: ReactChild }) => {
   const [currentTheme, setCurrentTheme] = useState<ThemeTypeEnum>(
     ThemeTypeEnum.light
   );
-  const [result, setResult] = useState<[]>([]);
+  const [result, setResult] = useState<ApiResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   let values: GlobalContextInterface = {
     currentTheme,
     setCurrentTheme,
     result,
     setResult,
+    loading,
+    setLoading,
   };
 
   return (
