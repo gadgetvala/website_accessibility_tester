49cfe466b93da0e507945599ecf8076b58252211
gadgetvala <surajj.verma7@gmail.com>
2021-12-08 12:13:48 +0530

    FEATURE: Added Feature To Render Details pf Pages

--- 3.nextjs/components/main/main.tsx
+++ 3.nextjs/components/main/main.tsx
@@ -3,9 +3,19 @@ import Header from "./../header/header";
 import { ThemeProvider } from "styled-components";
 import { GlobalContext } from "../../context/GlobalContext";
 import { darkTheme, lightTheme, GlobalStyles } from "../../styles/theme";
-import { MainContainer, WidthContainer, EmptyDataContainer } from "./style";
-import SearchHeader from "./../search_header/SearchHeader";
+import {
+  MainContainer,
+  WidthContainer,
+  EmptyDataContainer,
+  DisplayDetailsContainer,
+} from "./style";
+import Spinner from "../spinner/Spinner";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { Issue } from "./../../types/api_res_types";
 import { ThemeTypeEnum } from "../../types/themeTypes";
+import SearchHeader from "./../search_header/SearchHeader";
+import DetailsContainer from "../details_container/DetailsContainer";
 
 const Main: React.FC = () => {
   const context = useContext(GlobalContext);
@@ -18,16 +28,51 @@ const Main: React.FC = () => {
     >
       <GlobalStyles />
       <MainContainer>
+        <ToastContainer />
         <WidthContainer>
           <Header />
           <SearchHeader />
-          {context?.result.length === 0 ? (
+
+          {/* Show Loading Spinner. While Fetching Data */}
+          {context?.loading && (
+            <EmptyDataContainer>
+              <Spinner />
+            </EmptyDataContainer>
+          )}
+
+          {/* When User Has'nt Search Anything */}
+          {!context?.result && !context?.loading && (
             <EmptyDataContainer>
               <p>Try Checking Some Website</p>
             </EmptyDataContainer>
-          ) : (
-            "NULL"
           )}
+
+          {/* If There is Data and Data is Not loading show data */}
+          {context?.result?.status === "success" &&
+            context?.result?.data.issues.length &&
+            !context?.loading && (
+              <DisplayDetailsContainer>
+                <p>
+                  {context?.result?.data.documentTitle} :{" "}
+                  <span>
+                    {context?.result?.data.issues.length} Issues Found
+                  </span>
+                </p>
+
+                {context?.result?.data.issues.map((el: Issue, i: number) => {
+                  return (
+                    <DetailsContainer
+                      index={i}
+                      message={el.message}
+                      code={el.code}
+                      context={el.context}
+                      selector={el.selector}
+                    />
+                  );
+                })}
+                {/* <p>Try Checking Some Website</p> */}
+              </DisplayDetailsContainer>
+            )}
         </WidthContainer>
       </MainContainer>
     </ThemeProvider>
