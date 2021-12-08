94307938cabe8b1026210f727cb0c067ee302bfb
gadgetvala <surajj.verma7@gmail.com>
2021-12-08 12:40:41 +0530

    REFACTOR: Update Port

--- 1.server/src/server.ts
+++ 1.server/src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server Is started at port ${PORT || 3000}`);
